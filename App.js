2cc555f2edf919ef5609db2849d05244350b20c4
sachin0078 <68186774+sachin0078@users.noreply.github.com>
2021-11-11 18:59:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createSwitchNavigator,createAppContainer } from 'react-navigator';
+import LoadingScreen from './screens/LoadingScreen';
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import * as firebase from "firebase"
+import {firebaseConfig} from "./config"
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+});
+
+const AppNavigator = createAppContainer(AppSwitchNavigator);
+
+export default function App() {
+  return <AppNavigator />;
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8c6c69',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ navigation/TabNavigator.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { RFValue } from "react-native-responsive-fontsize";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Task from "../screens/Task";
+import firebase from "firebase";
+import AddTask from "../screens/AddTask";
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default class BottomTabNavigator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      light_theme: true
+    };
+  }
+
+  componentDidMount() {
+    let theme;
+    firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", function(snapshot) {
+        theme = snapshot.val().current_theme;
+      });
+    this.setState({ light_theme: theme === "light" ? true : false });
+  }
+
+  render() {
+    return (
+      <Tab.Navigator
+        labeled={false}
+        barStyle={
+          this.state.light_theme
+            ? styles.bottomTabStyleLight
+            : styles.bottomTabStyle
+        }
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Task") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "AddTask") {
+              iconName = focused ? "add-circle" : "add-circle-outline";
+            }
+            return (
+              <Ionicons
+                name={iconName}
+                size={RFValue(25)}
+                color={color}
+                style={styles.icons}
+              />
+            );
+          }
+        })}
+        activeColor={"#ee8249"}
+        inactiveColor={"gray"}
+      >
+        <Tab.Screen name="Task" component={Task} />
+        <Tab.Screen name="AddTask" component={AddTask} />
+      </Tab.Navigator>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "8%",
+    borderTopLeftRadius: RFValue(30),
+    borderTopRightRadius: RFValue(30),
+    overflow: "hidden",
+    position: "absolute"
+  },
+  bottomTabStyleLight: {
+    backgroundColor: "#eaeaea",
+    height: "8%",
+    borderTopLeftRadius: RFValue(30),
+    borderTopRightRadius: RFValue(30),
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
+  }
+});
